2578a5597043933ff614c04fc345ac4933238286
Iliayn Hristov <iliqn408@abv.bg>
2013-08-14 18:20:00 +0300

    BullsAndCows

---
+++ BullsAndCows-Doncho-Minkovvvv/BullsAndCows.WebClient/Scripts/controller.js
@@ -0,0 +1,227 @@
+﻿/// <reference path="class.js" />
+/// <reference path="persister.js" />
+/// <reference path="jquery-2.0.2.js" />
+/// <reference path="ui.js" />
+
+var controllers = (function () {
+	var rootUrl = "http://localhost:40643/api/";
+	var Controller = Class.create({
+		init: function () {
+			this.persister = persisters.get(rootUrl);
+		},
+
+        //Load UI depending if there is logged user or not.
+		loadUI: function (selector) {
+			if (this.persister.isUserLoggedIn()) {
+				this.loadGameUI(selector);
+			}
+			else {
+				this.loadLoginFormUI(selector);
+			}
+			this.attachUIEventHandlers(selector);
+		},
+        //Load UI if no user is logged in. (The main window of the application)
+		loadLoginFormUI: function (selector) {
+			var loginFormHtml = ui.loginForm()
+			$(selector).html(loginFormHtml);
+		},
+
+        //Load UI if an user is logged in.
+		loadGameUI: function (selector) {
+		    var list = ui.gameUI(this.persister.nickname());
+		    $(selector).html(list);
+
+			this.persister.game.open(function (games) {
+				var list = ui.openGamesList(games);
+				$(selector + " #open-games-container").append(list);
+			});
+
+			this.persister.game.myActive(function (games) {
+			    var list = ui.activeGamesList(games);
+			    $(selector + " #active-games-container").append(list);
+			});
+		},
+
+        //Loads the state of the games in progress which are inside the active game conatiner.
+		loadGame: function (selector, gameId) {	    
+		    this.persister.game.state(gameId, function (gameState) {	       
+				var gameHtml = ui.gameState(gameState);
+				$(selector + " #game-holder").html(gameHtml);
+
+		    });
+		},
+
+        //Event handlers
+		attachUIEventHandlers: function (selector) {
+			var wrapper = $(selector);
+			var self = this;
+
+            //Show login form when clicked
+			wrapper.on("click", "#btn-show-login", function () {
+				wrapper.find(".button.selected").removeClass("selected");
+				$(this).addClass("selected");
+				wrapper.find("#login-form").show();
+				wrapper.find("#register-form").hide();
+			});
+
+            //Show register form when clicked
+			wrapper.on("click", "#btn-show-register", function () {
+				wrapper.find(".button.selected").removeClass("selected");
+				$(this).addClass("selected");
+				wrapper.find("#register-form").show();
+				wrapper.find("#login-form").hide();
+			});
+
+            //User login
+			wrapper.on("click", "#btn-login", function () {
+				var user = {
+					username: $(selector + " #tb-login-username").val(),
+					password: $(selector + " #tb-login-password").val()
+				}
+
+				self.persister.user.login(user, function () {
+					self.loadGameUI(selector);
+				}, function () {
+				    wrapper.html("Wrong username or password");
+				});
+				return false;
+			});
+
+            //Registers new user
+			wrapper.on("click", "#btn-register", function () {
+			    var user = {
+			        username: $(selector + ' #tb-register-username').val(),
+			        nickname: $(selector + ' #tb-register-nickname').val(),
+                    password: $(selector + ' #tb-register-password').val()
+			    }
+			    self.persister.user.register(user, function () {
+			        self.loadGameUI(selector);
+			    }, function () {
+			        wrapper.html("Error, Failed to register user!");
+			    });
+			    return false;
+			});
+
+            //Logouts user and returns the initial UI for loging/registering new user.
+			wrapper.on("click", "#btn-logout", function () {
+				self.persister.user.logout(function () {
+					self.loadLoginFormUI(selector);
+				});
+			});
+
+            //Click on an open game
+			wrapper.on("click", "#open-games-container a", function () {
+				$("#game-join-inputs").remove();
+				var html = ui.joinGame();
+				$(this).next().after(html);
+			});
+
+            //Click on game join button from the open games menu
+			wrapper.on("click", "#btn-join-game", function () {
+				var game = {
+					number: $("#tb-game-number").val(),
+					gameId: $(this).parents("li").first().data("game-id")
+				};
+
+				var password = $("#tb-game-pass").val();
+
+				if (password) {
+					game.password = password;
+				}
+
+				self.persister.game.join(game);
+			});
+
+            //Click on the game create button
+			wrapper.on("click", "#btn-create-game", function () {
+				var game = {
+					title: $("#tb-create-title").val(),
+					number: $("#tb-create-number").val(),
+				}
+				var password = $("#tb-create-pass").val();
+				if (password) {
+					game.password = password;
+				}
+				self.persister.game.create(game, function () {
+				    alert("Successful operation");
+				}, function (err) {
+				    alert(JSON.stringify(err));
+				});
+			});
+
+		    //Click on active game which is in progress.
+			wrapper.on("click", ".active-games .in-progress", function () {
+			    self.loadGame(selector, $(this).parent().data("game-id"));
+			    return false;
+			});
+
+		    //Click on active game which is not in progress.
+			wrapper.on("click", ".active-games .full", function () {
+			    $("#btn-start-game").remove();
+			    var html = ui.startGame();
+			    $(this).parent().append(html);
+			    return false;
+			});
+
+            //Click on start game button.
+			wrapper.on("click", "#btn-start-game", function () {
+			    self.persister.game.start($(this).parent().data("game-id"), function () {
+			        alert("Successful start of the game");
+			    }, function (err) {
+			        alert(JSON.stringify(err));
+			    });
+			});
+
+		    //Click on make guess button
+			wrapper.on("click", "#btn-guess", function () {
+			    var game = {
+			        number: $("#tb-make-guess").val(),
+			        gameId: $(this).parents("div").first().data("game-id")
+			    };
+			    self.persister.guess.make(game, function () {
+			        alert("Successful guess");
+			    }, function (err) {
+			        var errObject = err["responseJSON"];
+			        alert(errObject["Message"]);
+			    });
+			});
+
+            //Show scores board button.//Success and error good to be implemented here not in the persister.
+			wrapper.on("click", "#btn-show-scores", function () {
+			    self.persister.user.scores();
+			});
+
+		    //Show all messages
+			wrapper.on("click", "#btn-show-all-messages", function () {
+			    $('#messages-list').remove();
+			    self.persister.messages.all(function (messages) {
+			        var list = ui.showMessages(messages);
+			        $(selector + " #messages-container").append(list);
+			    }, function (err) {
+			        alert(JSON.stringify(err));
+			    });
+			});
+
+		    //Show unread messages
+			wrapper.on("click", "#btn-show-unread-messages", function () {
+			    $('#messages-list').remove();
+			    self.persister.messages.unread(function (messages) {
+			        var list = ui.showMessages(messages);
+			        $(selector + " #messages-container").append(list);
+			    }, function (err) {
+			        alert(JSON.stringify(err));
+			    });
+			});
+		}
+	});
+	return {
+		get: function () {
+			return new Controller();
+		}
+	}
+}());
+
+$(function () {
+	var controller = controllers.get();
+	controller.loadUI("#content");
+});
---
+++ BullsAndCows-Doncho-Minkovvvv/BullsAndCows.WebClient/Scripts/ui.js
@@ -0,0 +1,204 @@
+﻿var ui = (function () {
+
+    //The UI when no user is logged in the game.
+	function buildLoginForm() {
+		var html =
+            '<div id="login-form-holder">' +
+				'<form>' +
+					'<div id="login-form">' +
+						'<label for="tb-login-username">Username: </label>' + 
+						'<input type="text" id="tb-login-username"><br />' + '<br/>' +
+						'<label for="tb-login-password">Password: </label>' + 
+						'<input type="password" id="tb-login-password"><br />' + '<br/>' +
+						'<button id="btn-login" class="button">Login</button>' + '<br/>' + '<br/>' +
+					'</div>' +
+					'<div id="register-form" style="display: none">' +
+						'<label for="tb-register-username">Username: </label>' +
+						'<input type="text" id="tb-register-username"><br />' + '<br/>' +
+						'<label for="tb-register-nickname">Nickname: </label>' +
+						'<input type="text" id="tb-register-nickname"><br />' + '<br/>' +
+						'<label for="tb-register-password">Password: </label>' +
+						'<input type="text" id="tb-register-password"><br />' + '<br/>' +
+						'<button id="btn-register" class="button">Register</button>' + '<br/>' + '<br/>' +
+					'</div>' +
+					'<a href="#" id="btn-show-login" class="button selected">Show Login Form</a>' + '&nbsp;&nbsp;&nbsp;' + 
+					'<a href="#" id="btn-show-register" class="button">Show Register Form</a>' + '<br/>' + '<br/>' +
+				'</form>' +
+            '</div>';
+		return html;
+	}
+
+    //The UI if an user has already been logged in.
+	function buildGameUI(nickname) {
+	    var html = 
+        '<span id="user-nickname">' + nickname + '</span>' + '&nbsp;&nbsp;&nbsp;' +
+		'<button id="btn-logout">Logout</button>' + '<br/> <br/>' +
+        '<button id="btn-show-scores">Show score board</button>' + '<br/> <br/>' +
+		'<fieldset id="create-game-holder">' +
+            '<legend>Create Game</legend>' + 
+			'Title: <input type="text" id="tb-create-title" />' + '&nbsp;' + '<br/>' + '<br/>' +
+			'Password: <input type="password" id="tb-create-pass" />' + '&nbsp;' + '<br/>' + '<br/>' +
+			'Number: <input type="text" id="tb-create-number" />' + '&nbsp;' + '<br/>' + '<br/>' +
+			'<button id="btn-create-game">Create</button>' + 
+		'</fieldset>' +
+
+		'<div id="open-games-container">' +
+			'<h2>Open Games</h2>' +
+		'</div>' +
+
+		'<div id="active-games-container">' +
+			'<h2>Active Games</h2>' +
+		'</div>' +
+
+		'<div id="game-holder">' +
+		'</div>' + 
+
+        '<div id="messages-container">' +
+           '<h2>Messages</h2>' +
+           '<button id="btn-show-unread-messages">Show unread</button>' + '&nbsp;&nbsp;' +
+           '<button id="btn-show-all-messages">Show all</button>' + '<br/>' +
+       '</div>';
+
+		return html;
+	}
+
+    //The UI for the Open games.
+	function buildOpenGamesList(games) {
+		var list = '<ul class="game-list open-games">';
+		for (var i = 0; i < games.length; i++) {
+			var game = games[i];
+			list +=
+				'<li data-game-id="' + game.id + '">' +
+					'<a href="#" >' +
+						$("<div />").html(game.title).text() +
+					'</a>' +
+					'<span> by ' +
+						game.creatorNickname +
+					'</span>' +
+				'</li>';
+		}
+		list += '</ul>';
+		return list;
+	}
+
+    //The UI for join game form.
+	function joinGame() {
+	    var html = '<div id="game-join-inputs">' +
+						'Number: <input type="text" id="tb-game-number"/>' + '<br/>' +
+						'Password: <input type="password" id="tb-game-pass"/>' + '<br/>' +
+						'<button id="btn-join-game">join</button>' + '<br/>' +
+					'</div>';
+	    return html;
+	}
+
+    //The UI if some of the active games is clicked(which are not in progress).
+	function startGame() {
+	    var html = '<button id="btn-start-game">Start</button>';
+	    return html;
+	}
+
+    //UI for the messages section in the application.
+	function showMessages(messages) {
+	    var list = '<ul id="messages-list">';
+	    for (var i = 0; i < messages.length; i++) {
+	        list += '<li>' + messages[i].text + '</li>';
+	    }
+	    list += '</ul>';
+	    return list;
+	}
+
+    //The UI for the active games.
+	function buildActiveGamesList(games) {
+		var gamesList = Array.prototype.slice.call(games, 0);
+		gamesList.sort(function (g1, g2) {
+			if (g1.status == g2.status) {
+				return g1.title > g2.title;
+			}
+			else
+			{
+				if (g1.status == "in-progress") {
+					return -1;
+				}
+			}
+			return 1;
+		});
+
+		var list = '<ul class="game-list active-games">';
+		for (var i = 0; i < gamesList.length; i++) {
+			var game = gamesList[i];
+			list +=
+				'<li data-game-id="' + game.id + '">' +
+					'<a href="#" class="' + game.status + '">' +
+						$("<div />").html(game.title).text() +
+					'</a>' +
+					'<span> by ' +
+						game.creatorNickname +
+					'</span>' +
+				'</li>';
+		}
+		list += "</ul>";
+		return list;
+	}
+
+    //The guess table UI.
+	function buildGuessTable(guesses) {
+		var tableHtml =
+			'<table border="1" cellspacing="0" cellpadding="5">' +
+				'<tr>' +
+					'<th>Number</th>' +
+					'<th>Cows</th>' +
+					'<th>Bulls</th>' +
+				'</tr>';
+		for (var i = 0; i < guesses.length; i++) {
+			var guess = guesses[i];
+			tableHtml +=
+				'<tr>' +
+					'<td>' +
+						guess.number +
+					'</td>' +
+					'<td>' +
+						guess.cows+
+					'</td>' +
+					'<td>' +
+						guess.bulls+
+					'</td>' +
+				'</tr>';
+		}
+		tableHtml += '</table>';
+		return tableHtml;
+	}
+
+    //Build the guesses section
+	function buildGameState(gameState) {
+		var html =
+			'<div id="game-state" data-game-id="' + gameState.id + '">' +
+				'<h2>' + gameState.title + '</h2>' +
+                'Number: <input type="text" id="tb-make-guess">' +
+                '<button id="btn-guess">Guess</button>' +
+				'<div id="blue-guesses" class="guess-holder">' +
+					'<h3>' +
+						gameState.blue + '\'s gueesses'+
+					'</h3>' +
+					buildGuessTable(gameState.blueGuesses) +
+				'</div>' +
+				'<div id="red-guesses" class="guess-holder">' +
+					'<h3>' +
+						gameState.red + '\'s gueesses' + 
+					'</h3>'+
+					buildGuessTable(gameState.redGuesses) +
+				'</div>' +
+		'</div>';
+		return html;
+	}
+
+	return {
+	    gameUI: buildGameUI,
+	    showMessages: showMessages,
+	    joinGame: joinGame,
+        startGame: startGame,
+		openGamesList: buildOpenGamesList,
+		loginForm: buildLoginForm,
+		activeGamesList: buildActiveGamesList,
+		gameState: buildGameState
+	}
+}());
