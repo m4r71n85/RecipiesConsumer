64628d3e9ddb57c9fbb712f6b4874236954bcec8
Elena Kirilova <el_b_k@mail.bg>
2013-08-15 12:23:30 +0300

    grid added

--- Recipies.WebClient/Recipies.WebClient/ScriptsKendo/main.js
+++ Recipies.WebClient/Recipies.WebClient/ScriptsKendo/main.js
@@ -4,87 +4,87 @@
 
 $(document).ready(function() {
    
-    panelBar = $("#panelbar").kendoPanelBar({
-        expandMode: "single",
-        select: onSelect,
-    }).data("kendoPanelBar");
+    //panelBar = $("#panelbar").kendoPanelBar({
+    //    expandMode: "single",
+    //    select: onSelect,
+    //}).data("kendoPanelBar");
 
+    var someData = [
+                    {
+                        RecipieName: "Some Recipie",
+                        Rating: 3,
+                        Url: "www.dajda.com",
+                        TimeForPreperation: 5,
+                        CreatedBy: "SomeOne"
+                    },
+                    {
+                        RecipieName: "Some Recipie",
+                        Rating: 3,
+                        Url: "www.dajda.com",
+                        TimeForPreperation: 5,
+                        CreatedBy: "SomeOne"
+                    },
+                    {
+                        RecipieName: "Some Recipie",
+                        Rating: 3,
+                        Url: "www.dajda.com",
+                        TimeForPreperation: 5,
+                        CreatedBy: "SomeOne"
+                    },
+                    {
+                        RecipieName: "Some Recipie",
+                        Rating: 3,
+                        Url: "www.dajda.com",
+                        TimeForPreperation: 5,
+                        CreatedBy: "SomeOne"
+                    },
+    ];
+
+    $("#grid").kendoGrid({
+        dataSource: {
+            data:
+                
+                someData
+            ,
+            schema: {
+                model: {
+                    fields: {
+                        RecipieName: { type: "string" },
+                        Rating: { type: "number" },
+                        Url: { type: "string" },
+                        TimeForPreperation: { type: "number" },
+                        CreatedBy: { type: "string" }
+                    }
+                }
+            },
+            pageSize: 5
+        },
+        height: 430,
+        scrollable: true,
+        sortable: true,
+        filterable: true,
+        pageable: {
+            input: true,
+            numeric: false
+        },
+        columns: [
+            { field: "RecipieName", title: "Recipie Name", width: "100px" },
+            "Rating",
+            "Url",
+            { field: "TimeForPreperation", title: "Time For Preperation in minutes", width: "50px" },
+           { field: "CreatedBy", title: "Created By", width: "100px" }
+        ],
+        dataSource: {
+            data: someData
+        }
+    });
     controller = controllers.get();
     controller.provider.user.scores();
- 
+
     function onSelect(e) {
         selectedGameId = $(e.item)[0].id;
         console.log(selectedGameId);
-        //if (!isNaN(selectedGameId)) {
-        //    var game = findById(games, selectedGameId);
-        //    if (game.status === "open") {
-        //        createKendoWindow("Join in Game Id: " + selectedGameId, "JoinGame").center().open();
-        //    } else if (game.status === "full") {
-        //        var content = 'Start the game.';
-        //        notify.create(content, game.id, '', game.status, e.item);
-        //    } else {
-        //        var gameWindow = createGameWindow("Active Game: " + game.title, selectedGameId);
-        //        controller.provider.game.state(game.id).then(function (result) {
-
-        //            var turns = [];
-        //            if (result.red === controller.provider.nickname()) {
-        //                var i, length = result.redGuesses.length;
-        //                for (i = length - 1; i >= 0; i--) {
-        //                    turns.push(result.redGuesses[i]);
-        //                }
-        //            } else {
-        //                var i, length = result.blueGuesses.length;
-        //                for (i = length - 1; i >= 0; i--) {
-        //                    turns.push(result.blueGuesses[i]);
-        //                }
-        //            }
-
-        //            var dataSourceTurns = new kendo.data.DataSource({
-        //                data: turns,
-        //                pageSize: 10
-        //            });
-
-        //            var viewModelTurns = kendo.observable({
-        //                dataSource: dataSourceTurns
-        //            });
-                    
-        //            dataSourceTurns.read();
-                    
-        //            $("#pager").kendoPager({
-        //                dataSource: dataSourceTurns
-        //            });
-                    
-        //            var pnlListTurns = $(gameWindow).find('#pnlListTurns');
-        //            pnlListTurns.kendoGrid({
-        //                dataSource: dataSourceTurns,
-        //                sortable: false,
-        //                pageable: {
-        //                    refresh: true,
-        //                    pageSizes: true
-        //                },
-                        
-        //            });//template: kendo.template($(gameWindow).find('#template').html())
-                    
-        //            gameWindow.data('kendoWindow').center().open();
-                    
-        //            kendo.bind(pnlListTurns, viewModelTurns);
-
-        //            game.gameWindow = gameWindow;
-        //            game.dataSourceTurns = dataSourceTurns;
-        //        });
-        //    }
-        //} else {
-            if ($(e.item).text().trim() === "Scores") {
-                controller.provider.user.scores().then(function (result) {
-                    var item = getItemByIndex(3);
-                    item.remove('> li');
-                    panelBar.select(getItemByIndex(3));
-                    for (var i = 0; i < result.length; i++) {
-                        panelBar.append('<li id="' + result[i].UserID + '" class="k-link">' + result[i].UserName + ': ' + result[i].NickName + '</li>', panelBar.select());
-                    }
-                    panelBar.expand(item);
-                });
-            }
-        //}
     }
 });
+
+   
